5f1bde1c231a7acab62a6a549f1f33c0bce282e4
mhshuvro <mainul.hasan.shuvro@gmail.com>
2020-12-02 14:02:39 +0100

    Dynamically output burger ingredients and added build control components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 
 import Auxilary from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
 
     render() {
         return (
             <Auxilary>
-                <Burger />
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
             </Auxilary>
         );
     }
