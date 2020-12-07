d7019e29b956560824c34b16a30f6bcece689a05
mhshuvro <mainul.hasan.shuvro@gmail.com>
2020-12-07 16:43:18 +0100

    Added routing between components throughout the portal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -102,29 +102,17 @@ class BurgerBuilder extends Component {
         })
     }
 
-    purchaseContinueHandler = () => {
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Shuvro',
-                address: {
-                    street: 'Test street 1',
-                    zipCode: '23445',
-                    country: 'Germany'
-                },
-                email: 'shuvro1226@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+    purchaseContinueHandler = () => {        
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + this.state.ingredients[i]);
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render() {
